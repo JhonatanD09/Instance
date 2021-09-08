73cb2d7803491e46133e1a9100935d9ec3bad1cb
JhonatanD09 <joranada@gmail.com>
2021-09-08 14:17:33 -0500

    connet to mysql

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express')
-
 const app = express()
+const mysql = require('mysql');
 
-app.listen(2000, () => {
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123',
+    database : 'test'
+});
+
+connection.connect(function(err) {
+    // en caso de error
+    if(err){
+        console.log(err.code);
+        console.log(err.fatal);
+    }
+});
+
+app.get('/' , function(req, res){
+    res.send("HOLA")
+})
+
+app.listen(4500, () => {
     console.log(`Example app listening at http://localhost:4500`)
 })
